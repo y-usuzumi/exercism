74380cc17e6b81c61018ac31e352425517d25d59
Yukio Usuzumi <anohigisavay@gmail.com>
2024-11-10 03:30:00 -0800

    Solution for Phone number

--- haskell/phone-number/src/Phone.hs
+++ haskell/phone-number/src/Phone.hs
@@ -1,29 +1,89 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE TupleSections #-}
 
 module Phone (number) where
 
-import qualified Data.List as L
+import Control.Applicative (Alternative (..))
+import Control.Arrow
+import Control.Monad
 
-newtype Parser s = Parser {unParser :: String -> (s, String)}
+newtype Parser a = Parser
+  {runParser :: String -> Maybe (a, String)}
 
 instance Functor Parser where
-  f `fmap` Parser g = Parser $ \s -> let (r, remaining) = g s in (f r, remaining)
+  fmap f (Parser p) = Parser (fmap (first f) . p)
 
 instance Applicative Parser where
-  pure val = Parser (val,)
-  Parser f <*> Parser g = Parser $ \s ->
-    let (r, remaining) = g s
-     in let (f', remaining') = f remaining in (f' r, remaining')
+  pure a = Parser $ \s -> Just (a, s)
+  (<*>) = ap
 
 instance Monad Parser where
-  return = pure
-  Parser g >>= f = Parser $ \s ->
-    let (val, remaining) = g s
-        Parser f' = f val
-     in f' remaining
+  Parser a >>= f = Parser $ \s -> case a s of
+    Just (a', remaining) -> runParser (f a') remaining
+    Nothing -> Nothing
 
-take :: Int -> Parser String
-take n = Parser $ L.splitAt n
+instance Alternative Parser where
+  empty = Parser $ const Nothing
+  Parser a <|> Parser b = Parser $ \s -> a s <|> b s
+
+takeOne :: Parser Char
+takeOne = Parser $ \case
+  [] -> Nothing
+  (x : xs) -> Just (x, xs)
+
+char :: Char -> Parser Char
+char a = do
+  ch <- takeOne
+  if ch == a then return ch else empty
+
+optional :: Parser a -> Parser (Maybe a)
+optional a = (Just <$> a) <|> return Nothing
+
+optional_ :: Parser a -> Parser ()
+optional_ a = void (optional a)
+
+clean :: Parser ()
+clean = void $ many (allBut "0123456789")
+
+eof :: Parser ()
+eof = Parser $ \case
+  [] -> Just ((), [])
+  _ -> Nothing
+
+anyOf :: [Char] -> Parser Char
+anyOf chs = do
+  ch <- takeOne
+  if ch `elem` chs then return ch else empty
+
+allBut :: [Char] -> Parser Char
+allBut chs = do
+  ch <- takeOne
+  if ch `elem` chs then empty else return ch
 
 number :: String -> Maybe String
-number xs = error "You need to implement this function."
+number xs =
+  fst
+    <$> runParser
+      ( do
+          optional_ (optional (char '+') >> char '1')
+          clean
+          d1 <- n
+          d2 <- x
+          d3 <- x
+          clean
+          d4 <- n
+          d5 <- x
+          d6 <- x
+          clean
+          d7 <- x
+          d8 <- x
+          d9 <- x
+          d10 <- x
+          clean
+          eof
+          return [d1, d2, d3, d4, d5, d6, d7, d8, d9, d10]
+      )
+      xs
+  where
+    n = anyOf "23456789"
+    x = anyOf "0123456789"

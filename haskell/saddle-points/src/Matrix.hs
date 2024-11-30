bbf79234daf486875c8d246d1432fcec6657dcd0
Yukio Usuzumi <anohigisavay@gmail.com>
2024-11-30 00:02:47 -0800

    Solution for Saddle Point

--- haskell/saddle-points/src/Matrix.hs
+++ haskell/saddle-points/src/Matrix.hs
@@ -1,6 +1,41 @@
-module Matrix (saddlePoints) where
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TupleSections #-}
 
-import Data.Array (Array)
+module Matrix (saddlePoints, findSaddleLine) where
 
-saddlePoints :: Array i e -> [i]
-saddlePoints matrix = error "You need to implement this function."
+import Control.Monad
+import qualified Data.Array as A
+import Data.List
+
+findSaddleLine :: (Int -> Int -> Bool) -> Int -> [Int] -> [Int]
+findSaddleLine cmp startIdx line = map fst $ _findSaddleLine line startIdx
+  where
+    _findSaddleLine [] _ = []
+    _findSaddleLine (x : xs) idx =
+      let tailResult = _findSaddleLine xs (idx + 1)
+       in case tailResult of
+            [] -> [(idx, x)]
+            (t : _)
+              | snd t == x -> (idx, x) : tailResult
+              | cmp x (snd t) -> [(idx, x)]
+              | otherwise -> tailResult
+
+saddlePoints :: A.Array (Int, Int) Int -> [(Int, Int)]
+saddlePoints matrix =
+  let ((rowLBound, colLBound), (rowUBound, colUBound)) = A.bounds matrix
+      saddleRows = join $ map (\idx -> map (idx,) $ findSaddleLine (>) colLBound $ getRow idx) [rowLBound .. rowUBound]
+      saddleCols = sort $ join $ map (\idx -> map (,idx) $ findSaddleLine (<) rowLBound $ getCol idx) [colLBound .. colUBound]
+   in findSaddlePoints saddleRows saddleCols
+  where
+    getRow idx =
+      let ((_, colLBound), (_, colUBound)) = A.bounds matrix
+       in map ((matrix A.!) . (idx,)) [colLBound .. colUBound]
+    getCol idx =
+      let ((rowLBound, _), (rowUBound, _)) = A.bounds matrix
+       in map ((matrix A.!) . (,idx)) [rowLBound .. rowUBound]
+    findSaddlePoints [] _ = []
+    findSaddlePoints _ [] = []
+    findSaddlePoints (a : as) (b : bs)
+      | a < b = findSaddlePoints as (b : bs)
+      | a > b = findSaddlePoints (a : as) bs
+      | otherwise = a : findSaddlePoints as bs

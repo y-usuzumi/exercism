1ad96d1a9f709a6103fd9e802e19eef9e9a5b353
Yukio Usuzumi <anohigisavay@gmail.com>
2024-12-04 11:18:12 -0800

    Bowling is incomplete. Cleaning up

--- haskell/bowling/src/Bowling.hs
+++
@@ -1,9 +0,0 @@
-module Bowling (score, BowlingError(..)) where
-
-data BowlingError = IncompleteGame
-                  | InvalidRoll { rollIndex :: Int, rollValue :: Int }
-  deriving (Eq, Show)
-
-score :: [Int] -> Either BowlingError Int
-score rolls = error "You need to implement this function."
-
